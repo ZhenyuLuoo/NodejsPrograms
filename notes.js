dd6374efdc8a862080a48ee3e798eeb2fd9315cb
ZhenyuLuoo <55060796+ZhenyuLuoo@users.noreply.github.com>
2020-09-03 09:44:18 -0700

    Add files via upload
    Week2 Coding

---
+++ notes.js
@@ -0,0 +1,5 @@
+const notes = function() {
+    return 'This is my note.'
+}
+
+module.exports = notes
---
+++ utils.js
@@ -0,0 +1,10 @@
+console.log('utils.js')
+
+const name = 'Lucas'
+
+const add = function(a, b) {
+    return a + b
+}
+
+//Get the return value for utils.js
+module.exports = add
